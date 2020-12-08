f39c491acf2ccadbcfb8334ed4135f9b91b6e388
dominicdaviescodes <codewithdom@gmail.com>
2020-12-08 19:06:15 +0000

    add logic in js for the spans

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// grab element
+// grab all the labels (get a node list)
+
+const labels = document.querySelectorAll('.form-control');
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    // split into an array
+    .split('')
+    // turn into a different array (turn each letter into a span/transition delay)
+    .map((letter, idx) => `<span>${letter}</span>`)
+    // want to turn back into a string
+    .join('');
+});
