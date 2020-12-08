fe30db7f08533080af3df4538f56230b98d43509
dominicdaviescodes <codewithdom@gmail.com>
2020-12-08 19:14:19 +0000

    add transition dealy in logic

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ labels.forEach((label) => {
     // split into an array
     .split('')
     // turn into a different array (turn each letter into a span/transition delay)
-    .map((letter, idx) => `<span>${letter}</span>`)
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
     // want to turn back into a string
     .join('');
 });
